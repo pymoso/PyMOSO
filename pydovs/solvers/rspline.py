a05ec9729bbf41734f964ab4151fc81d71cd2151
kyle cooper <kylescooper@gmail.com>
2018-10-04 10:42:24 -0400

    error check the R-SPLINE warm start

--- pydovs/solvers/rspline.py
+++ pydovs/solvers/rspline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from ..chnbase import RASolver
+import sys
 
 
 class RSPLINE(RASolver):
@@ -8,7 +9,10 @@ class RSPLINE(RASolver):
     def spsolve(self, warm_start):
         """Use SPLINE to get a sample path minimizer."""
         # warm_start is a singleton set, so extract the item
-        self.upsample(warm_start)
+        warm_start = self.upsample(warm_start)
+        if not warm_start:
+            print('-- RSPLINE Error: Empty warm start. Is x0 feasible?')
+            sys.exit()
         ws = warm_start.pop()
         _, xmin, _, _ = self.spline(ws)
         # return a singleton set
